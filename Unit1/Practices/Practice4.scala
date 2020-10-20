04231c9a21a1cec2cd3d334a97429675e12fdd45
AlejandroOliver12 <71462239+AlejandroOliver12@users.noreply.github.com>
2020-10-20 02:16:24 -0700

    Update Practice4.scala

--- Unit1/Practices/Practice4.scala
+++ Unit1/Practices/Practice4.scala
@@ -20,17 +20,18 @@ fib(10)
   def fib(n:Int):Int={
     a=0
     b=1
-    
     for (k<- Range(0,n)){
-    
-        b=b+a
-        a=b-a
-        
-    }
-        
+    b=b+a
+     a=b-a
+    }   
     return a
-    }
-
-
+  }
 fib(10)
+    
+        
+    
+    
+
+
+
 
